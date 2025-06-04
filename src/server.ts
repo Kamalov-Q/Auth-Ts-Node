34ae21c08164809f22e9c6eea8890e88eca10690
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-06-04 14:36:35 +0500

    Changes applied

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,10 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+app.get("/test", (req, res) => {
+  res.send("Server is running Testing");
+})
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
